e139d01f7d4d2c5617b6acfa6d2716dc0a1f139e
Kourosh Maissamyazad <2440557@edu.vaniercollege.qc.ca>
2025-09-22 09:17:39 -0400

    implemented method to calculate expenses

--- src/lab04_kourosh/Lab04_Kourosh.java
+++ src/lab04_kourosh/Lab04_Kourosh.java
@@ -4,6 +4,10 @@
  */
 package lab04_kourosh;
 import javafx.application.Application;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 /**
@@ -11,16 +15,49 @@ import javafx.stage.Stage;
  * @author 2440557
  */
 public class Lab04_Kourosh extends Application{
-
+    private final double Meal_Per_Day = 37;
+    private final double Parking_Per_Day = 10;
+    private final double Taxi_Per_Day = 20;
+    private final double Lodging_Per_Day = 95;
+    private final double Per_Mile_Driven = 0.27;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         launch(args);
     }
-
+    
+    /**
+     * method to calculate Incurred and Allowed  expenses 
+     * @param numberOfDays
+     * @param airfare
+     * @param rentalFees
+     * @param milesdriven
+     * @param parkingFees
+     * @param taxiFees
+     * @param registration
+     * @param lodging
+     * @return an array of double containing both expenses
+     */
+    private double[] CalcExpenses(int numberOfDays, double airfare, double rentalFees, double milesdriven, double parkingFees, double taxiFees, double registration, double lodging) {
+        double Incurred = (double) (airfare + rentalFees + (milesdriven * Per_Mile_Driven) + parkingFees + taxiFees + registration + (lodging * lodging));
+        double Allowed = (double) ((numberOfDays * Meal_Per_Day) + (numberOfDays * Parking_Per_Day) + (numberOfDays * Taxi_Per_Day) + (numberOfDays * Lodging_Per_Day) + (milesdriven * Per_Mile_Driven));
+        
+        return new double[]{Incurred, Allowed};
+    };
+    
+    
     @Override
     public void start(Stage stage) {
+        //initializing GridPane and BorderPane
+        GridPane grid = new GridPane();
+        BorderPane root = new BorderPane();
+        
+        //adding nodes to gridpane
+        Label nbDays = new Label("Number of days on the trip");
+        TextField nbDays = new TextField();
+        
+        
         stage.show();
     }
     
