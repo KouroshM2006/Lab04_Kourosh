6f1e329ffe8e9599fe4d7edf008b5d75c9f0942a
Kourosh Maissamyazad <2440557@edu.vaniercollege.qc.ca>
2025-09-22 08:16:29 -0400

    project initialized

---
+++ src/lab04_kourosh/Lab04_Kourosh.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab04_kourosh;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author 2440557
+ */
+public class Lab04_Kourosh extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        stage.show();
+    }
+    
+}
